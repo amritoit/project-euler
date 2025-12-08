37496f7b94b972279c732c8d74c8cf30b05841c5
Amritendu Mondal <amritoit@gmail.com>
2025-12-07 22:50:30 -0800

    Create problem-38.py

---
+++ python/problem-38.py
@@ -0,0 +1,19 @@
+def compute():
+    max = 0
+    for base in range(1,10000):
+        concat = ''
+        for i in range(1,10):
+            concat += str(base*i)
+            if(len(concat) == 9 and is_pandigital_1_to_9(concat)):
+                print(concat)
+                if(max < int(concat)):
+                    max = int(concat)
+            if(len(concat)>9):
+                break
+    return max
+
+def is_pandigital_1_to_9(num: str):
+    return sorted(num) ==  ['1','2','3','4','5','6','7','8','9']
+            
+
+print("max pandigital is:", compute())
